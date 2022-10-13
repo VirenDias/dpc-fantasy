0e35fbc96ea7e662344c84c1453b467d3bddbeb1
Viren Dias <viren.dias@gmail.com>
2022-10-13 17:19:04 +0530

    Implemented an analytical solution for calculating period summaries

--- src/calculate-averages.R
+++ src/calculate-averages.R
@@ -3,7 +3,7 @@ source("src/get-match-data.R")
 library(tidyverse)
 library(RcppAlgos)
 
-summarise_exponentially <- function(
+calculate_exponential_summary <- function(
     values, 
     ranks = NULL,
     alpha = NULL, 
@@ -33,70 +33,7 @@ summarise_exponentially <- function(
   return(summary)
 }
 
-permute_series <- function(outcomes, points, best_of) {
-  if (length(points) < best_of) return(NA)
-  
-  rank_digits = nchar(length(points) - 1)
-  ranks <- sprintf(paste0("%0", rank_digits, "d"), (length(points) - 1):0)
-  
-  # Determine all possible permutations of the data
-  if (best_of == 1) {
-    permutations <- list("ranks" = as.numeric(ranks), "points" = points)
-  } else if (best_of == 2) {
-    permutations <- comboGeneral(
-      v = paste0(ranks, points), 
-      m = best_of, 
-      FUN = function(x) {
-        ## Calculate the total points for the series and the associated rank
-        mean_rank <- mean(as.numeric(substr(x, 1, rank_digits)))
-        total_points <- sum(
-          as.numeric(sub(paste0("^.{", rank_digits, "}"), "", x))
-        )
-        
-        return(list("rank" = mean_rank, "points" = total_points))
-      }
-    )
-    
-    permutations <- list(
-      "ranks" = sapply(permutations, function(x) x$rank),
-      "points" = sapply(permutations, function(x) x$points)
-    )
-  } else {
-    best <- ceiling(best_of / 2)
-    permutations <- permuteGeneral(
-      v = paste0(ranks, outcomes, points),
-      m = best_of,
-      FUN = function(x) {
-        ## Only consider partial series if the required number of wins is met
-        for (i in best:best_of) {
-          partial_x <- x[1:i]
-          partial_wins <- sum(
-            as.numeric(substr(partial_x, rank_digits + 1, rank_digits + 1))
-          )
-          if (partial_wins == best | (i - partial_wins) == best) break
-        }
-        
-        ## Calculate the total points for the series and the associated rank
-        partial_points <- as.numeric(
-          sub(paste0("^.{", rank_digits + 1, "}"), "", partial_x)
-        )
-        total_points <- sum(sort(partial_points, decreasing = TRUE)[1:best])
-        mean_rank <- mean(as.numeric(substr(partial_x, 1, rank_digits)))
-        
-        return(list("rank" = mean_rank, "points" = total_points))
-      }
-    )
-    
-    permutations <- list(
-      "ranks" = sapply(permutations, function(x) x$rank),
-      "points" = sapply(permutations, function(x) x$points)
-    )
-  }
-  
-  return(permutations)
-}
-
-average_series <- function(
+calculate_period_summary <- function(
     outcomes, 
     points, 
     bas_bo1 = 0, 
@@ -108,219 +45,165 @@ average_series <- function(
     pot_bo3 = 0, 
     pot_bo5 = 0,
     exponential = FALSE,
-    exact = FALSE,
     progress = NULL
 ) {
   if (!is.null(progress)) {
     message(paste0("Calculating summaries (", progress, ")"))
   }
   
-  # Calculate Bo1 permutation, average, and standard deviation
-  perm_bo1 <- permute_series(outcomes, points, best_of = 1)
+  # Calculate average, standard deviation, and win rate
   if (exponential) {
-    avg <- summarise_exponentially(
-      perm_bo1$points, 
-      ranks = perm_bo1$ranks, 
+    avg <- calculate_exponential_summary(values = points, func = "average")
+    std <- calculate_exponential_summary(values = points, func = "stddev")
+    win_rate <- calculate_exponential_summary(
+      values = outcomes, 
       func = "average"
     )
-    std <- summarise_exponentially(
-      perm_bo1$points, 
-      ranks = perm_bo1$ranks,
-      func = "stddev"
-    )
   } else {
-    avg <- mean(perm_bo1$points)
-    std <- sd(perm_bo1$points)
-  }
-
-  # Calculate Bo2, Bo3, and Bo5 permutations if necessary
-  perm_bo2 <- if (bas_bo2 > 0 | pot_bo2 > 0) {
-    permute_series(outcomes, points, best_of = 2)
-  } else {
-    list("ranks" = NA, "points" = NA)
-  }
-  perm_bo3 <- if (bas_bo3 > 0 | pot_bo3 > 0) {
-    permute_series(outcomes, points, best_of = 3)
-  } else {
-    list("ranks" = NA, "points" = NA)
-  }
-  perm_bo5 <- if (bas_bo5 > 0 | pot_bo5 > 0) {
-    permute_series(outcomes, points, best_of = 5)
-  } else {
-    list("ranks" = NA, "points" = NA)
+    avg <- mean(points)
+    std <- sd(points)
+    win_rate <- mean(outcomes)
   }
   
-  if (exact) {
-    # Calculate the baseline expectation
-    series <- list(
-      "ranks" = c(
-        rep(list(perm_bo1$ranks), bas_bo1),
-        rep(list(perm_bo2$ranks), bas_bo2),
-        rep(list(perm_bo3$ranks), bas_bo3),
-        rep(list(perm_bo5$ranks), bas_bo5)
-      ),
-      "points" = c(
-        rep(list(perm_bo1$points), bas_bo1),
-        rep(list(perm_bo2$points), bas_bo2),
-        rep(list(perm_bo3$points), bas_bo3),
-        rep(list(perm_bo5$points), bas_bo5)
-      )
-    )
-    if(length(series) != 0) {
-      points <- do.call(
-        what = expand_grid, 
-        args = setNames(object = series$points, nm = 1:length(series$points))
-      ) %>%
-        transmute(max = pmax(!!!.)) %>%
-        pull(max)
-      
-      if (exponential) {
-        ranks <- do.call(
-          what = expand_grid, 
-          args = setNames(object = series$ranks, nm = 1:length(series$ranks))
-        ) %>%
-          transmute(mean = rowMeans(.)) %>%
-          pull(mean)
-        
-        bas_exp <- summarise_exponentially(points, ranks = ranks)
-      } else {
-        bas_exp <- points %>% mean()
-      }
-    } else {
-      bas_exp <- as.numeric(NA)
+  # Calculate series distributions
+  ## Calculate order statistic distributions
+  order_statistic_distribution <- function(r, n, avg = 0, std = 1) {
+    integ_avg <- function(x, r, n, avg = 0, std = 1) {
+      x * 
+        (factorial(n) / (factorial(r - 1) * factorial(n - r))) * 
+        ((1 - pnorm(x, avg, std, lower.tail = FALSE))^(r - 1)) * 
+        (pnorm(x, avg, std, lower.tail = FALSE)^(n - r)) * dnorm(x, avg, std)
     }
+    result_avg <- integrate(integ_avg, -Inf, Inf, r, n, avg, std)$value
     
-    # Calculate the potential expectation
-    if (pot_bo1 > 0 | pot_bo2 > 0 | pot_bo3 > 0 | pot_bo5 > 0) {
-      series <- list(
-        "ranks" = c(
-          rep(list(perm_bo1$ranks), bas_bo1 + pot_bo1),
-          rep(list(perm_bo2$ranks), bas_bo2 + pot_bo2),
-          rep(list(perm_bo3$ranks), bas_bo3 + pot_bo3),
-          rep(list(perm_bo5$ranks), bas_bo5 + pot_bo5)
-        ),
-        "points" = c(
-          rep(list(perm_bo1$points), bas_bo1 + pot_bo1),
-          rep(list(perm_bo2$points), bas_bo2 + pot_bo2),
-          rep(list(perm_bo3$points), bas_bo3 + pot_bo3),
-          rep(list(perm_bo5$points), bas_bo5 + pot_bo5)
-        )
-      )
-      points <- do.call(
-        what = expand_grid, 
-        args = setNames(object = series$points, nm = 1:length(series$points))
-      ) %>%
-        transmute(max = pmax(!!!.)) %>%
-        pull(max)
-      
-      if (exponential) {
-        ranks <- do.call(
-          what = expand_grid, 
-          args = setNames(object = series$ranks, nm = 1:length(series$ranks))
-        ) %>%
-          transmute(mean = rowMeans(.)) %>%
-          pull(mean)
-        
-        pot_exp <- summarise_exponentially(points, ranks = ranks)
-      } else {
-        pot_exp <- points %>% mean()
-      }
-    } else {
-      pot_exp <- bas_exp
+    integ_var <- function(x, r, n, avg = 0, std = 1) {
+      (x - result_avg)^2 * 
+        (factorial(n) / (factorial(r - 1) * factorial(n - r))) * 
+        ((1 - pnorm(x, avg, std, lower.tail = FALSE))^(r - 1)) *
+        (pnorm(x, avg, std, lower.tail = FALSE)^(n - r)) * dnorm(x, avg, std)
     }
-  } else {
-    set.seed(2022)
-    make_series <- function(series, points, ranks, reps) {
-      if (reps > 0) {
-        for (i in 1:reps) {
-          sample <- if (exponential) {
-            rnorm(
-              n = 100000, 
-              mean = summarise_exponentially(points, ranks, func = "average"), 
-              sd = summarise_exponentially(points, ranks, func = "stddev")
-            )
+    result_var <- integrate(integ_var, -Inf, Inf, r, n, avg, std)$value
+    
+    return(tibble("avg" = result_avg, "var" = result_var))
+  }
+  
+  series_distribution <- function(win_rate, avg, std, best_of) {
+    thresh <- list("1" = 1, "2" = 2, "3" = 2, "5" = 3)[[as.character(best_of)]]
+    
+    ## Calculate series length probabilities
+    length_probabilities <- permuteGeneral(
+      v = c("W", "L"),
+      m = best_of,
+      repetition = TRUE,
+      FUN = function(x) {
+        prob <- win_rate^sum(x == "W") * (1 - win_rate)^sum(x == "L")
+        for (i in thresh:best_of) {
+          partial_x <- x[1:i]
+          partial_wins <- if (best_of == 2) {
+            length(partial_x)
           } else {
-            rnorm(n = 100000, mean = mean(points), sd = sd(points))
+            sum(partial_x == "W")
           }
-          series <- c(series, list(sample))
+          if (partial_wins == thresh | (i - partial_wins) == thresh) {
+            return(
+              tibble(
+                num_matches = length(partial_x), 
+                prob = prob
+              )
+            )
+          }
         }
       }
-      return(series)
-    }
+    ) %>%
+      bind_rows() %>%
+      group_by(num_matches) %>% 
+      summarise(prob = sum(prob))
     
-    # Calculate the baseline expectation
-    series <- c()
-    series <- make_series(
-      series = series, 
-      points = perm_bo1$points, 
-      ranks = perm_bo1$ranks, 
-      reps = bas_bo1
-    )
-    series <- make_series(
-      series = series, 
-      points = perm_bo2$points, 
-      ranks = perm_bo2$ranks, 
-      reps = bas_bo2
-    )
-    series <- make_series(
-      series = series, 
-      points = perm_bo3$points, 
-      ranks = perm_bo3$ranks, 
-      reps = bas_bo3
-    )
-    series <- make_series(
-      series = series, 
-      points = perm_bo5$points, 
-      ranks = perm_bo5$ranks, 
-      reps = bas_bo5
-    )
-    
-    if(length(series) != 0) {
-      bas_exp <- do.call(
-        what = pmax, 
-        args = series
+    ## Calculate combined order statistic distributions
+    length_probabilities %>%
+      rowwise() %>%
+      mutate(
+        lapply(
+          tail(1:num_matches, thresh),
+          order_statistic_distribution,
+          n = num_matches,
+          avg = avg,
+          std = std
+        ) %>%
+          bind_rows() %>%
+          summarise(avg = sum(avg), var = sum(var))
       ) %>%
-        mean()
-    } else {
-      bas_exp <- as.numeric(NA)
-    }
-    
-    # Calculate the potential expectation
-    if (pot_bo1 > 0 | pot_bo2 > 0 | pot_bo3 > 0 | pot_bo5 > 0) {
-      series <- c()
-      series <- make_series(
-        series = series, 
-        points = perm_bo1$points, 
-        ranks = perm_bo1$ranks,
-        reps = bas_bo1 + pot_bo1
-      )
-      series <- make_series(
-        series = series, 
-        points = perm_bo2$points, 
-        ranks = perm_bo2$ranks,
-        reps = bas_bo2 + pot_bo2
-      )
-      series <- make_series(
-        series = series, 
-        points = perm_bo3$points, 
-        ranks = perm_bo3$ranks,
-        reps = bas_bo3 + pot_bo3
-      )
-      series <- make_series(
-        series = series, 
-        points = perm_bo5$points, 
-        ranks = perm_bo5$ranks,
-        reps = bas_bo5 + pot_bo5
-      )
-      
-      pot_exp <- do.call(
-        what = pmax, 
-        args = series
+      group_by() %>%
+      summarise(avg = sum(prob * avg), std = sqrt(sum(prob * var)))
+  }
+  
+  dist_bo1 <- series_distribution(win_rate, avg, std, best_of = 1)
+  dist_bo2 <- series_distribution(win_rate, avg, std, best_of = 2)
+  dist_bo3 <- series_distribution(win_rate, avg, std, best_of = 3)
+  dist_bo5 <- series_distribution(win_rate, avg, std, best_of = 5)
+  
+  # Calculate period expectation
+  ## Calculate combined same series type distributions
+  same_series_distribution <- function(num, dist) {
+    if (num > 0) {
+      order_statistic_distribution(
+        r = num, 
+        n = num, 
+        avg = dist$avg, 
+        std = dist$std
       ) %>%
-        mean()
+        transmute(avg, std = sqrt(var))
     } else {
-      pot_exp <- bas_exp
+      NULL
     }
+  }
+  
+  bas_dists <- list(
+    "bo1" <- same_series_distribution(bas_bo1, dist_bo1),
+    "bo2" <- same_series_distribution(bas_bo2, dist_bo2),
+    "bo3" <- same_series_distribution(bas_bo3, dist_bo3),
+    "bo5" <- same_series_distribution(bas_bo5, dist_bo5)
+  ) %>%
+    .[lapply(., is.null) == FALSE]
+  pot_dists <- list(
+    "bo1" <- same_series_distribution(bas_bo1 + pot_bo1, dist_bo1),
+    "bo2" <- same_series_distribution(bas_bo2 + pot_bo2, dist_bo2),
+    "bo3" <- same_series_distribution(bas_bo3 + pot_bo3, dist_bo3),
+    "bo5" <- same_series_distribution(bas_bo5 + pot_bo5, dist_bo5)
+  ) %>%
+    .[lapply(., is.null) == FALSE]
+  
+  ## Calculate combined different series type distributions
+  different_series_expectation_2 <- function(dist1, dist2) {
+    avg1 <- dist1$avg
+    std1 <- dist1$std
+    avg2 <- dist2$avg
+    std2 <- dist2$std
+    
+    avg1 * pnorm((avg1 - avg2) / sqrt(std1^2 + std2^2)) +
+      avg2 * pnorm((avg2 - avg1) / sqrt(std1^2 + std2^2)) +
+      sqrt(std1^2 + std2^2) * dnorm((avg1 - avg2) / sqrt(std1^2 + std2^2))
+  }
+  
+  bas_exp <- if (length(bas_dists) == 1) {
+    bas_dists[[1]]$avg
+  } else if (length(bas_dists) == 2) {
+    different_series_expectation_2(
+      dist1 = bas_dists[[1]],
+      dist2 = bas_dists[[2]]
+    )
+  } else {
+    NA
+  }
+  pot_exp <- if (length(pot_dists) == 1) {
+    pot_dists[[1]]$avg
+  } else if (length(pot_dists) == 2) {
+    different_series_expectation_2(
+      dist1 = pot_dists[[1]],
+      dist2 = pot_dists[[2]]
+    )
+  } else {
+    NA
   }
   
   return(
@@ -344,6 +227,7 @@ calculate_summaries <- function(
   message(paste0("Calculating summaries for league ID ", league_id))
   
   # Get required data
+  ## Must be ordered from oldest to newest
   if (innate_data_only) {
     matches <- get_result_data(
       league_id = league_id, 
@@ -358,24 +242,26 @@ calculate_summaries <- function(
     )
   }
   
-  # Calculate the single match averages
-  averages <- if (exponential) {
+  # Calculate the single match summaries
+  summaries <- if (exponential) {
     matches %>%
       group_by(player_id) %>%
-      summarise(across(.cols = kills:total, .fns = summarise_exponentially))
+      summarise(
+        across(.cols = kills:total, .fns = calculate_exponential_summary)
+      )
   } else {
     matches %>%
       group_by(player_id) %>%
       summarise(across(.cols = kills:total, .fns = ~mean(., na.rm = TRUE)))
   }
   
-  # Calculate the series averages
+  # Calculate the period summaries
   if (!is.null(player_series)) {
-    series_averages <- matches %>%
+    period_summaries <- matches %>%
       right_join(player_series, by = "player_id") %>%
       group_by(player_id) %>%
       summarise(
-        average_series(
+        calculate_period_summary(
           outcomes = win,
           points = total,
           bas_bo1 = max(bas_bo1, 0),
@@ -390,8 +276,8 @@ calculate_summaries <- function(
           progress = paste0(cur_group_id(), "/", length(unique(.$player_id)))
         )
       )
-    averages <- averages %>% right_join(series_averages, by = "player_id")
+    summaries <- summaries %>% right_join(period_summaries, by = "player_id")
   }
   
-  return(averages)
+  return(summaries)
 }
