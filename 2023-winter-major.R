c3a63b256316d4fcf5e9f3b7d6255d44befefc65
Viren Dias <viren.dias@gmail.com>
2023-02-26 08:20:04 +0530

    Bugfix for 2023 Winter Major data

--- 2023-winter-major.R
+++ 2023-winter-major.R
@@ -59,7 +59,8 @@ lapply(names(league_ids), function(league_name) {
     google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
     work_sheet = league_name,
     league_id = league_ids[[league_name]],
-    update = TRUE
+    exponential = TRUE,
+    update = FALSE
   )
 })
 
@@ -76,121 +77,135 @@ lapply(names(league_ids), function(league_name) {
 ## Period 1
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_1$start_time,
   end_time = period_dates$period_1$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p1.txt"
+  file_path = "data/posts/2023_winter_major_p1.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 2
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_2$start_time,
   end_time = period_dates$period_2$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p2.txt"
+  file_path = "data/posts/2023_winter_major_p2.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 3
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_3$start_time,
   end_time = period_dates$period_3$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p3.txt"
+  file_path = "data/posts/2023_winter_major_p3.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 4
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_4$start_time,
   end_time = period_dates$period_4$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p4.txt"
+  file_path = "data/posts/2023_winter_major_p4.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 5
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_5$start_time,
   end_time = period_dates$period_5$end_time,
-  tiebreaker = TRUE,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p5.txt"
+  file_path = "data/posts/2023_winter_major_p5.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  tiebreaker = TRUE,
+  tiebreaker_ranks = c(4, 6),
+  tie_best_of = 1,
+  tie_2_best_of = 3,
+  update = FALSE
 )
 
 ## Period 6
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_6$start_time,
   end_time = period_dates$period_6$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p6.txt"
+  file_path = "data/posts/2023_winter_major_p6.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 7
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_7$start_time,
   end_time = period_dates$period_7$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p7.txt"
+  file_path = "data/posts/2023_winter_major_p7.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 8
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_8$start_time,
   end_time = period_dates$period_8$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p8.txt"
+  file_path = "data/posts/2023_winter_major_p8.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 9
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_9$start_time,
   end_time = period_dates$period_9$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p9.txt"
+  file_path = "data/posts/2023_winter_major_p9.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 10
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_10$start_time,
   end_time = period_dates$period_10$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p10.txt"
+  file_path = "data/posts/2023_winter_major_p10.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
 
 ## Period 11
 print_post(
   league_ids = league_ids,
-  update = FALSE,
-  innate_data_only = FALSE,
   start_time = period_dates$period_11$start_time,
   end_time = period_dates$period_11$end_time,
   google_sheet = "11ExiDnIYbupgsjuSbr9zeaBTXb_xn2N9uyvyD0Gz1bc",
-  file_path = "data/posts/2023_winter_major_p11.txt"
+  file_path = "data/posts/2023_winter_major_p11.txt",
+  innate_data_only = FALSE,
+  exponential = TRUE,
+  update = FALSE
 )
