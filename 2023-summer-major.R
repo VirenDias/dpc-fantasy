3e42770e6df350811b42530d1e1fb3bc7d629de9
Viren Dias <viren.dias@gmail.com>
2023-06-28 13:07:12 +0530

    Updated roster lock times for Summer Major 2023

--- 2023-summer-major.R
+++ 2023-summer-major.R
@@ -8,44 +8,44 @@ league_ids <- list("Summer Major 2023" = 15438)
 
 period_dates <- list(
   "period_1" = list(
-    "start_time" = as.POSIXct("2023-06-29 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-06-30 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-06-29 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-06-30 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_2" = list(
-    "start_time" = as.POSIXct("2023-06-30 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-01 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-06-30 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-01 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_3" = list(
-    "start_time" = as.POSIXct("2023-07-01 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-02 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-01 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-02 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_4" = list(
-    "start_time" = as.POSIXct("2023-07-02 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-03 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-02 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-03 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_5" = list(
-    "start_time" = as.POSIXct("2023-07-03 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-05 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-03 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-05 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_6" = list(
-    "start_time" = as.POSIXct("2023-07-05 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-06 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-05 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-06 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_7" = list(
-    "start_time" = as.POSIXct("2023-07-06 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-07 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-06 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-07 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_8" = list(
-    "start_time" = as.POSIXct("2023-07-07 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-08 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-07 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-08 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_9" = list(
-    "start_time" = as.POSIXct("2023-07-08 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-09 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-08 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-09 01:55", tz = "UTC") %>% as.integer()
   ),
   "period_10" = list(
-    "start_time" = as.POSIXct("2023-07-09 00:00", tz = "UTC") %>% as.integer(),
-    "end_time" = as.POSIXct("2023-07-10 00:00", tz = "UTC") %>% as.integer()
+    "start_time" = as.POSIXct("2023-07-09 01:55", tz = "UTC") %>% as.integer(),
+    "end_time" = as.POSIXct("2023-07-10 01:55", tz = "UTC") %>% as.integer()
   )
 )
 
